fd7f74afb40a032eb3423769d38c803f5f119087
zhang8330 <zhangx8330@126.com>
2024-10-09 23:15:56 +0800

    测试

--- app.vue
+++ app.vue
@@ -1,6 +1,15 @@
+<script setup>
+  const name = ref("aaa");
+  console.log("哈哈哈1");
+  const test = ()=>{
+    console.log("test执行");
+  }
+  for(let i = 1;i <= 2;i++)alert(`执行了${i}次`);
+</script>
+
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    {{name}}
+    <button type="primary" @click="test">click</button>
   </div>
 </template>
